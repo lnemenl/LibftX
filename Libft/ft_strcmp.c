04ea16dde7bf783f8b6041f488b73876c7a06fe5
Ruslan <ruslankhakimullin@gmail.com>
2025-03-06 08:40:03 +0200

    updated strcmp function

--- Libft/ft_strcmp.c
+++ Libft/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: rkhakimu <rkhakimu@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/05 15:22:59 by rkhakimu          #+#    #+#             */
-/*   Updated: 2025/03/05 15:23:24 by rkhakimu         ###   ########.fr       */
+/*   Updated: 2025/03/06 08:39:39 by rkhakimu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,10 @@
 
 int	ft_strcmp(const char *s1, const char *s2)
 {
-	unsigned char	c1;
-	unsigned char	c2;
-
-	while (1)
-	{
-		c1 = (unsigned char) *s1;
-		c2 = (unsigned char) *s2;
-		if (c1 != c2)
-			return (c1 - c2);
-		if (c1 == '\0')
-			return (0);
-		s1++;
-		s2++;
-	}
-	return (0);
+    while (*s1 && *s1 == *s2)
+    {
+        s1++;
+        s2++;
+    }
+    return ((unsigned char)*s1 - (unsigned char)*s2);
 }
